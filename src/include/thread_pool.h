fbfb36a5807b276c6ccf5a24ee8c85a4d4ab8fea
John Patek <jpatek@ubiqsecurity.com>
2019-07-30 01:10:56 -0700

    Starting redesign in header files

--- src/include/thread_pool.h
+++ src/include/thread_pool.h
@@ -18,47 +18,24 @@
 #include <stdexcept>
 
 #include "blocking_queue.h"
-
-namespace cpp
+namespace thread_pool
 {
 
-    class thread_pool
-    {
-    public:
-        using size_type = std::size_t;
+template<class F,class... Args> class callable_task
+{
 
-        thread_pool(const size_type pool_size = std::thread::hardware_concurrency());
+};
 
-        ~thread_pool();
+class runnable_task
+{
 
-        size_type size() const;
+};
 
-        bool is_active() const;
+class thread_pool
+{
 
-        void execute(std::function<void()> function);
-        
-        template<class F, class... Args> auto submit(F&& f, Args&&... args)->std::future<typename std::result_of<F(Args...)>::type>
-        {
-            using return_type = typename std::result_of < F(Args...)>::type;
+};
 
-            auto task = std::make_shared < std::packaged_task < return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-
-            std::future<return_type> result = task->get_future();
-            m_task_queue.put([task]()
-            {
-                (*task)();
-            });
-
-            return result;
-        }
-
-        void shutdown();
-
-    private:
-        std::vector<std::thread> m_threads;
-        cpp::blocking_queue<std::function<void() >> m_task_queue;
-        bool m_active;
-    };
 }
 #endif /* THREAD_POOL_H */
 
